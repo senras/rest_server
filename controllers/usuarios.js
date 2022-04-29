992bca151e6739d8cd09292a5b24a09bfd8e2202
Ignacio Senra <isenras@hotmail.com>
2022-04-29 14:10:25 -0300

    PUT fixed

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -23,27 +23,19 @@ const usuariosGet = async (req, res = response) => {
 const usuariosPut = async (req, res = response) => {
 	const { id } = req.params;
 	const { _id, password, google, correo, ...resto } = req.body;
-	// console.log('Estos son los req.params', req.params);
-	// console.log('Estos son los req.body', req.body);
 	//Validar contra DB
 	if (password) {
 		const salt = bcryptjs.genSaltSync();
 		resto.password = bcryptjs.hashSync(password, salt);
 	}
-	const usuario = await Usuario.findOneAndUpdate(id, resto);
+	const usuario = await Usuario.findByIdAndUpdate(id, resto);
 
 	res.json({
 		usuario,
 	});
 };
 
-const usuariosPatch = (req, res = response) => {
-	res.json({
-		msg: 'patch API - controlador',
-	});
-};
-
-const usuariosPost = async (req, res) => {
+const usuariosPost = async (req, res = response) => {
 	const { nombre, correo, password, rol } = req.body;
 	const nuevoUsuario = new Usuario({ nombre, correo, password, rol });
 
@@ -56,11 +48,17 @@ const usuariosPost = async (req, res) => {
 	});
 };
 
-const usuariosDelete = async (req, res) => {
+const usuariosDelete = async (req, res = response) => {
 	const { id } = req.params;
 	const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
 	res.json({
 		usuario,
+	});
+};
+
+const usuariosPatch = (req, res = response) => {
+	res.json({
+		msg: 'patch API - controlador',
 	});
 };
 
