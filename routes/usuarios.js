f7f8ca51465747327dc737560531f5f1bd310c78
Ignacio Senra <isenras@hotmail.com>
2022-04-30 17:42:28 -0300

    dependencias actualizadas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,14 +1,18 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { RoleValidator, EmailValidator, UserByIdValidator } = require('../helpers/db-validators');
+const {
+  RoleValidator,
+  EmailValidator,
+  UserByIdValidator,
+} = require('../helpers/db-validators');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const {
-	usuariosGet,
-	usuariosPut,
-	usuariosPost,
-	usuariosDelete,
-	usuariosPatch,
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
 } = require('../controllers/usuarios');
 
 const router = Router();
@@ -16,37 +20,39 @@ const router = Router();
 router.get('/', usuariosGet);
 
 router.put(
-	'/:id',
-	[
-		check('id', 'No es un ID válido').isMongoId(),
-		check('id').custom(UserByIdValidator),
-		check('rol').custom((rol) => RoleValidator(rol)),
-		validarCampos,
-	],
-	usuariosPut,
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(UserByIdValidator),
+    check('rol').custom((rol) => RoleValidator(rol)),
+    validarCampos,
+  ],
+  usuariosPut
 );
 
 router.post(
-	'/',
-	[
-		check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-		check('password', 'La contraseña debe ser de más de 6 carácteres').isLength({ min: 6 }),
-		check('correo', 'El correo no es válido.').isEmail(),
-		check('rol').custom((rol) => RoleValidator(rol)),
-		check('correo').custom((correo) => EmailValidator(correo)),
-		validarCampos,
-	],
-	usuariosPost,
+  '/',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe ser de más de 6 carácteres').isLength(
+      { min: 6 }
+    ),
+    check('correo', 'El correo no es válido.').isEmail(),
+    check('rol').custom((rol) => RoleValidator(rol)),
+    check('correo').custom((correo) => EmailValidator(correo)),
+    validarCampos,
+  ],
+  usuariosPost
 );
 
 router.delete(
-	'/:id',
-	[
-		check('id', 'No es un ID válido').isMongoId(),
-		check('id').custom(UserByIdValidator),
-		validarCampos,
-	],
-	usuariosDelete,
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(UserByIdValidator),
+    validarCampos,
+  ],
+  usuariosDelete
 );
 
 router.patch('/', usuariosPatch);
