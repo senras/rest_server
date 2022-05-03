3b3f07dd6c06300263edadef906157991936eb9c
Ignacio Senra <isenras@hotmail.com>
2022-05-03 01:29:06 -0300

    JWT added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,49 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
+const { generarJWT } = require('../helpers/generar-jwt');
 
-const login = (req, res = response) => {
-  res.json({
-    msg: 'Login',
-  });
+const login = async (req, res = response) => {
+	const { correo, password } = req.body;
+	try {
+		// Ver si el email existe
+		const usuario = await Usuario.findOne({ correo: correo });
+		if (!usuario) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - correo',
+			});
+		}
+
+		//Ver si el usuario está activo
+		if (!usuario.estado) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - estado:false',
+			});
+		}
+
+		const validPassword = bcryptjs.compareSync(password, usuario.password); //Verificar la contraseña
+		if (!validPassword) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - password',
+			});
+		}
+
+		//Generar el JWT
+
+		const token = await generarJWT(usuario.id);
+
+		res.json({
+			usuario,
+			token,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			msg: 'Hable con el administrador',
+		});
+	}
 };
 
 module.exports = {
-  login,
+	login,
 };
