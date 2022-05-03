0254b865d47e4c183edfdc73c0134a3a783b8ffa
Ignacio Senra <isenras@hotmail.com>
2022-05-03 16:18:24 -0300

    Validar jwt en proceso

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,5 @@
+const jwt = require('jsonwebtoken');
+
+const validarJWT = () => {};
+
+module.exports = { validarJWT };
--- models/usuario.js
+++ models/usuario.js
@@ -33,6 +33,7 @@ const UsuarioSchema = Schema({
 
 UsuarioSchema.methods.toJSON = function () {
 	const { __v, password, _id, ...usuario } = this.toObject();
+	usuario.uid = _id;
 	return usuario;
 };
 module.exports = model('Usuario', UsuarioSchema);
